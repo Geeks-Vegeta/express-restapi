4cec37e9eb58612bdf144e69fcb35e8b63f62cbc
Geeks-Vegeta <shreyasmohite786@gmail.com>
2021-12-21 01:09:06 +0530

    connection change

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb+srv://shreyas:shreyas@cluster0.81mai.mongodb.net/mernwork?retryWrites=true&w=majority",{  useNewUrlParser: true,useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.DB_URI,{  useNewUrlParser: true,useUnifiedTopology: true }).then(()=>{
     console.log("connected successfully")
 }).catch((err)=>{
     console.log(err)
